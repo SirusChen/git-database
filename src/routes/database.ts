b73093fd963d93af3741710a359cd27e4f7dfee1
siruschen <siruschen@foxmail.com>
2024-12-29 23:46:25 +0800

    feat: 对接 gitee api 上传

--- src/routes/database.ts
+++ src/routes/database.ts
@@ -1,6 +1,9 @@
 import { Router } from 'express';
+import { Database } from '../utils/database';
+import axios from 'axios';
 
 const dataRouter = Router();
+const db = new Database('xbookmark.json');
 
 dataRouter.get('/v1/get', (req, res) => {
   res.status(200).json({
@@ -9,17 +12,31 @@ dataRouter.get('/v1/get', (req, res) => {
 });
 
 // 读取当前整个 bookmark 状态
-dataRouter.get('/v1/load', (req, res) => {
-  res.status(200).json({
-    data: 'dataRouter123',
-  }).end();
+dataRouter.get('/v1/getDBInfo', async (req, res) => {
+  console.log(db.tableFile);
+  res.end();
+});
+
+dataRouter.get('/v1/updateDB', async (req, res) => {
+  db.updateDB();
+  res.end();
+});
+
+dataRouter.get('/v1/addFile', async (req, res) => {
+  const url = req.query.url as string;
+  if (typeof url !== 'string') {
+    return res.end();
+  }
+  console.log('sirus', url);
+  // 拉取图片资源
+  const resp = await axios({
+    method: 'get',
+    url,
+    responseType: 'arraybuffer' // Important to get the image as a binary buffer
+  })
+  const base64Image = Buffer.from(resp.data, 'binary').toString('base64');
+  db.addFile(base64Image, `${Date.now()}.png`);
+  res.end();
 });
 
 export default Router().use('/data', dataRouter);
-
-/**
- * 读取数据库内容
- */
-function loadDatabaseInfo() {
-
-}
--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,13 +1,115 @@
-class Database {
+import axios from "axios"
+import { GiteeConfig } from "./database.config"
+import FormData from "form-data";
+
+const {owner, repo, access_token, branch} = GiteeConfig;
+
+interface TableInfo {
+  name: string
+  content: string
+  sha: string
+  /** 文件 raw 地址 */
+  download_url: string
+}
+
+interface TableFile {
+  updateTime?: number,
+  data: any[];
+}
+
+export class Database {
   tableName: string
-  tableInfo: string | undefined
-  tableFile: string | undefined
+  tableInfo: TableInfo | undefined
+  tableFile: TableFile | undefined
   constructor(tableName: string) {
     this.tableName = tableName
+    // this.getDBInfo();
   }
-  // 加载 db info
-  // 加载 db file
-  // 获取 db 完整信息
-  // 更新 table 内容
-  // 新增 item
+  /** 加载 db info */
+  async getDBInfo() {
+    const resp = await axios.get(
+      `https://gitee.com/api/v5/repos/${owner}/${repo}/contents/${this.tableName}`,
+      {
+        params: {
+          access_token,
+        }
+      }
+    )
+    if (resp.status === 200) {
+      this.tableInfo = resp.data;
+      if (resp.data.content) {
+        try {
+          this.tableFile = JSON.parse(atob(resp.data.content));
+        } catch (e) {
+          console.warn('getDBInfo parse conten error!', e);
+        }
+      }
+    }
+  }
+  /**
+   * 加载 db raw file
+   * * 一般只用 getDBInfo 即可
+   */
+  async getDBFile() {
+    const resp = await axios.get(
+      `https://gitee.com/api/v5/repos/${owner}/${repo}/raw/${this.tableName}`,
+      {
+        params: {
+          access_token,
+        }
+      }
+    )
+    try {
+      this.tableFile = resp.data;
+    } catch (e) {
+      console.warn('getDBFile error!', e);
+    }
+  }
+  /**
+   * 更新 table 内容
+   */
+  async updateDB() {
+    if (!this.tableInfo?.sha || !this.tableFile) {
+      console.warn('updateDB error no table info !', this.tableInfo, this.tableFile);
+      return;
+    }
+    this.tableFile.updateTime = Date.now();
+    const resp = await axios.put(
+      `https://gitee.com/api/v5/repos/${owner}/${repo}/contents/${this.tableName}`,
+      {
+        access_token,
+        content: btoa(JSON.stringify(this.tableFile)),
+        sha: this.tableInfo.sha,
+        message: `update in ${new Date()}`,
+        branch,
+      }
+    )
+    if (resp.status === 200) {
+      const data = resp.data;
+      this.tableInfo.sha = data.content.sha;
+    }
+  }
+  /**
+   * 新增 item
+   */
+  async addFile(base64: string, fileName: string) {
+    const data = new FormData();
+    data.append('access_token', access_token);
+    data.append('content', base64);
+    data.append('message', `add file in ${new Date()}`);
+
+    const resp = await axios.post(
+      `https://gitee.com/api/v5/repos/${owner}/${repo}/contents/${fileName}`,
+      data,
+      {
+        headers: data.getHeaders(),
+      }
+    );
+
+    if ([200, 201].includes(resp.status)) {
+      //
+    } else {
+      console.warn('add file error !', resp.data);
+    }
+  }
 }
